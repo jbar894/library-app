59b2cca30e118c1ab78b41087c994226da95338b
jbar894 <j.barwellsmith@gmail.com>
2018-06-23 18:38:35 +1200

    added 3rd party api calls

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,8 +1,27 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
+
+const parser = xml2js.Parser({ explicitArray: false });
 
 function goodreadsService() {
-  function getBookById() {
+  function getBookById(id) {
     return new Promise((resolve, reject) => {
-      resolve({ description: 'our description' });
+      axios.get(`https://www.goodreads.com/book/show/${id}.xml?key=WqGzRw2gH6nkp464NUNg`)
+        .then((response) => {
+          parser.parseString(response.data, (err, result) => {
+            if (err) {
+              debug(err);
+            } else {
+              debug(result);
+              resolve(result.GoodreadsResponse.book);
+            }
+          });
+        })
+        .catch((error) => {
+          reject(error);
+          debug(error);
+        });
     });
   }
 
